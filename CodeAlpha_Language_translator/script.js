0500ce09adcea1d7f0d264e678555f2aa70cfd67
Namita Giri <namitagiri783@gmail.com>
2026-03-09 15:12:10 +0545

    Add files via upload

---
+++ CodeAlpha_Language_translator/script.js
@@ -0,0 +1,15 @@
+async function translateText() {
+    let text = document.getElementById("inputText").value;
+    let source = document.getElementById("sourceLang").value;
+    let target = document.getElementById("targetLang").value;
+    let url = "https://api.mymemory.translated.net/get?q=" + text + "&langpair=" + source + "|" + target;
+    let response = await fetch(url);
+    let data = await response.json();
+    document.getElementById("outputText").value = data.responseData.translatedText;
+}
+function copyText() {
+    let copyText = document.getElementById("outputText");
+    copyText.select();
+    document.execCommand("copy");
+    alert("Text copied!");
+}
